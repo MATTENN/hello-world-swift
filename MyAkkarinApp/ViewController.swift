67e2af2db01b61565bd4ff63a10a5f500ce82239
akkarin <at.mattenn@gmail.com>
2018-12-05 10:28:51 +0900

    Initial Commit

---
+++ MyAkkarinApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAkkarinApp
+//
+//  Created by 友利 奈緒 on 2018/12/05.
+//  Copyright © 2018年 友利 奈緒. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
